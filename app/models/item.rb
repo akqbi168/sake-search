666eb4156551e022d0bf724b455ef17b061450bc
akqbi168 <akqbi168@gmail.com>
2019-03-14 06:26:58 +0000

    modify top views

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,2 +1,18 @@
 class Item < ApplicationRecord
+
+
+
+
+
+
+    def self.search(search)
+     if search
+        album_result = Cd.where("album LIKE ? ", "%#{search}%")
+        anime_result = Cd.joins(:anime).where("anime_title LIKE ?", "%#{search}%")
+        song_result = Cd.joins(discs: :songs).where("title LIKE ?", "%#{search}%")
+        result = album_result | anime_result | song_result
+        #和集合
+        return result
+     end
+    end
 end
