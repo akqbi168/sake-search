2859c1b6d91a747f77b6a12f19b39d64b56bb6a8
akqbi168 <akqbi168@gmail.com>
2019-03-19 23:49:46 +0900

    modify migration

--- db/migrate/20190312141440_create_items.rb
+++ db/migrate/20190312141440_create_items.rb
@@ -1,11 +1,11 @@
 class CreateItems < ActiveRecord::Migration[5.2]
   def change
     create_table :items do |t|
-      t.text :image_id
+      t.text :image
       t.string :name
       t.text :introduction
-      t.integer :sake_parameter     # 日本酒度
-      t.integer :acid_parameter     # 酸度
+      t.float :sake_parameter     # 日本酒度
+      t. :acid_parameter     # 酸度
       t.integer :category_id        # 辛口/甘口など
       t.integer :made_in_id         # 産地
 
