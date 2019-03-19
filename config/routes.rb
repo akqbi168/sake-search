8122d668e67baf438885e19cf6478005ab12e550
akqbi168 <akqbi168@gmail.com>
2019-03-19 23:46:05 +0900

    modify stores_controllers

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,10 @@
 Rails.application.routes.draw do
+  get 'stores/index'
+  get 'stores/new'
+  get 'stores/create'
+  get 'stores/show'
+  get 'stores/edit'
+  get 'stores/update'
+  get 'stores/destroy'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
