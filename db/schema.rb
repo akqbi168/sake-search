dce829eec6bfcb9d08eb748525a400f6270e9638
akqbi168 <akqbi168@gmail.com>
2019-03-19 22:10:28 +0900

    fix db

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_03_19_081134) do
+ActiveRecord::Schema.define(version: 2019_03_12_155452) do
 
   create_table "items", force: :cascade do |t|
     t.string "image"
