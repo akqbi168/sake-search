afba14d369c0cf0cd1e9b7d73e37b71566bd66b8
akqbi168 <akqbi168@gmail.com>
2019-03-20 18:26:53 +0900

    modify seeds.rb

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,22 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+Item.create!(
+  image: "nanbu-bijin-tokujun.png",
+  name: "南部美人",
+  sake_parameter: 4.0,
+  acid_parameter: 1.5,
+  category_id: "3: 特別純米酒",
+  made_in_id: "03: 岩手県",
+  introduction: "フレッシュで飲みごたえのある味わいです。南部美人を飲んだことのある方もない方も、昨年の生酒を飲んだ方も「今年の」特別純米を飲み比べてみてください。"
+)
+
+
+Store.create!(
+  image: "スクリーンショット_2019-03-11_18.19.36.png",
+  name: "検閲",
+  introduction: "店舗紹介です。",
+  address: "中央区銀座",
+  link: "http://www.aaa.com",
+)
