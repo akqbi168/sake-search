30010cb844a2267c39b55988090de58d79b3272b
akqbi168 <akqbi168@gmail.com>
2019-04-01 04:19:18 +0900

    add validation

--- app/models/store.rb
+++ app/models/store.rb
@@ -3,7 +3,8 @@ class Store < ApplicationRecord
   has_and_belongs_to_many :items
 
   # バリデーション
-    # validates :name, presence: true
+    validates :store_name, presence: true
+    validates :address, presence: true
 
   # 画像アップロード用
     mount_uploader :image, ImageUploader
--- app/models/tag.rb
+++ app/models/tag.rb
@@ -2,4 +2,7 @@ class Tag < ApplicationRecord
 
   has_and_belongs_to_many :items
 
+  # バリデーション
+  validates :tag_name, presence: true
+
 end
