b91656450406cdd16c3d7ed9ba79c74cbdc8f739
akqbi168 <akqbi168@gmail.com>
2019-03-31 21:00:26 +0900

    change settings for deploy

--- db/migrate/20190312141440_create_items.rb
+++ db/migrate/20190312141440_create_items.rb
@@ -1,6 +1,5 @@
 class CreateItems < ActiveRecord::Migration[5.2]
   def change
-    # create_table :items do |t|
     create_table :items, :options => 'ENGINE=InnoDB ROW_FORMAT=DYNAMIC' do |t|
       t.string :image
       t.string :name, null: false
